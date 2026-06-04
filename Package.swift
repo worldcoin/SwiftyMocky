091d830d45f639cdf6357145c421edbe2f75925c
Vladimir Burdukov <vladimir.burdukov@toolsforhumanity.com>
2026-06-04 15:15:38 +0300

    Fixed deprecated dependencies signatures in Package.swift

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
         .executable(name: "swiftymocky", targets: ["SwiftyMockyCLI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut", .upToNextMajor(from: "2.3.0")),
-        .package(url: "https://github.com/tuist/xcodeproj", .upToNextMajor(from: "8.3.1")),
-        .package(url: "https://github.com/luoxiu/Chalk", .exact("0.2.1")),
-        .package(url: "https://github.com/kylef/Commander", .upToNextMajor(from: "0.9.1")),
-        .package(url: "https://github.com/kylef/PathKit", .upToNextMajor(from: "1.0.1")),
-        .package(url: "https://github.com/jpsim/Yams", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.3.0"),
+        .package(url: "https://github.com/tuist/xcodeproj", from: "8.3.1"),
+        .package(url: "https://github.com/luoxiu/Chalk", exact: "0.2.1"),
+        .package(url: "https://github.com/kylef/Commander", from: "0.9.1"),
+        .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"),
+        .package(url: "https://github.com/jpsim/Yams", from: "5.0.1"),
     ],
     targets: [
         // XCTest Runtime libraries
